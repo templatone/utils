08b8e96a84f425902a6b855a1763657a1cd2e95b
Adam Josefus <adamjosefus@gmail.com>
2021-05-20 18:56:29 +0200

    Typo

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 export { BezierEasing } from './BezierEasing.js';
+export { Images } from './Images.js';
 export { Loaders } from './Loaders.js';
 export { Numbers } from './Numbers.js';
 export { Oscillators } from './Oscillators.js';
 export { Regex } from './Regex.js';
 export { Strings } from './Strings.js';
-export { Images } from './Images.js';
