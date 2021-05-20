acca8b3e4301a4124473c5cb0cb826f1f2bfb650
Adam Josefus <adamjosefus@gmail.com>
2021-05-20 17:37:55 +0200

    Add types

---
+++ dist/Oscillators.d.ts
@@ -0,0 +1,6 @@
+export declare class Oscillators {
+    static sawtooth(interator: number, frequency: number, min?: number, max?: number): number;
+    static linear(interator: number, frequency: number, min?: number, max?: number): number;
+    static sinus(interator: number, frequency: number, min?: number, max?: number): number;
+    static cosinus(interator: number, frequency: number, min?: number, max?: number): number;
+}
---
+++ dist/Strings.d.ts
@@ -0,0 +1,4 @@
+export declare class Strings {
+    static padLeft(s: string, length: number, pad: string): string;
+    static padRight(s: string, length: number, pad: string): string;
+}
